a2d95d86a0927e0086939732800e543b2682b627
3472204455ilia <68978403+3472204455ilia@users.noreply.github.com>
2020-11-17 23:48:56 -0500

    Initialize script

---
+++ src/test/java/pages/MainPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class MainPage {
+    private final WebDriver driver;
+
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    
+}
+
